d1106a84038cad038c15e1b3ed00eb930d628945
DOGWANNA <9401ndk@naver.com>
2023-05-09 20:22:32 +0900

    feat: 2480 풀이 추가

--- src/조건문/Q2480/Main.java
+++ src/조건문/Q2480/Main.java
@@ -3,7 +3,6 @@ package 조건문.Q2480;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,22 +11,21 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        ArrayList<Integer> arr = new ArrayList<>();
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        while (st.hasMoreTokens()){
-            arr.add(Integer.parseInt(st.nextToken()));
+        // 조건을 순서대로 나열한다.
+        if(a == b && a == c){ // 모두 같을 경우
+            sb.append(10000 + a * 1000);
+        }else if(a == b || a == c){ // 2개 같을 경우
+            sb.append(1000 + a * 100);
+        }else if(b == c){ // 2개 같을 경우
+            sb.append(1000 + b * 100);
+        }else{ // 모두 다를 경우
+            sb.append(Math.max(a, Math.max(b,c)) * 100);
         }
 
-        for (int i : arr) {
-            for(){
-
-            }
-        }
-        int sameCnt = 0;
-
-
-
-//        sb.append();
         System.out.println(sb);
         br.close();
     }
