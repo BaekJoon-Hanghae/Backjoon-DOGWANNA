93e7fdfcaeb88291f028cf995e711b082be57bd9
DOGWANNA <9401ndk@naver.com>
2023-05-18 15:07:39 +0900

    feat: 27866 풀이 추가

---
+++ src/문자열/Q27866/Main.java
@@ -0,0 +1,19 @@
+package 문자열.Q27866;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        sb.append(S.charAt(i-1));
+        System.out.println(sb);
+        br.close();
+    }
+}
