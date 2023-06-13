505c14858f1c7104a45cf475923209c2428ad477
DOGWANNA <9401ndk@naver.com>
2023-06-13 22:50:34 +0900

    feat: 3003 풀이 추가. 문제 이해하고 넘어가기

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,34 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-//        sb.append();
-        System.out.println(sb);
-        br.close();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringBuilder sb = new StringBuilder();
+////        sb.append();
+//        System.out.println(sb);
+//        br.close();
+
+        int a = 3000;
+        int b = 5000;
+        int budget = 23000;
+        int result = 0;
+        int length = budget / a;
+
+        for(int i=1; i<=length; i++){
+            int total = budget;
+            total -= a*i;
+
+            if(total == 0){
+                result += 1;
+                continue;
+            }
+            if(total < b){
+                continue;
+            }
+            if (total % b == 0){
+                result += 1;
+            }
+        }
+        System.out.println(result);
     }
 }
