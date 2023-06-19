852b545ef51993e87204f3a1f402a75bfd15b67b
DOGWANNA <9401ndk@naver.com>
2023-06-19 18:30:18 +0900

    feat: 3003 풀이 추가. 문제 이해하고 넘어가기

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 //        StringBuilder sb = new StringBuilder();
 ////        sb.append();
 //        System.out.println(sb);
-//        br.close();
+//        br.close();d
 
         int a = 3000;
         int b = 5000;
