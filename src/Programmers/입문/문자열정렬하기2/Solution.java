84bf640861a7acf7a981f81e6f18d1c59e1710d0
DOGWANNA <9401ndk@naver.com>
2023-06-28 13:16:53 +0900

    feat: 프로그래머스 입문 합성수 찾기 풀이 추가

--- src/Programmers/입문/문자열정렬하기2/Solution.java
+++ src/Programmers/입문/문자열정렬하기2/Solution.java
@@ -1,27 +1,20 @@
 package Programmers.입문.문자열정렬하기2;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
             Solution s = new Solution();
 
-            int result = s.solution(15);
+            String result = s.solution("Bcad");
         System.out.println(result);
         }
 
-    public int solution(int n) {
-        int answer = 0;
+    public String solution(String my_string) {
+        my_string = my_string.toLowerCase();
+        char[] arr = my_string.toCharArray();
+        Arrays.sort(arr);
 
-        for(int i=1; i<=n; i++) {
-            int cnt = 0;
-            for(int j=1; j<=i; j++){
-                if(i%j==0){
-                    cnt++;
-                }
-            }
-            if(cnt >= 3){
-                answer++;
-            }
-        }
-        return answer;
+        return String.valueOf(arr);
     }
 }
---
+++ src/Programmers/입문/합성수찾기/Solution.java
@@ -0,0 +1,27 @@
+package Programmers.입문.합성수찾기;
+
+public class Solution {
+    public static void main(String[] args) {
+            Solution s = new Solution();
+
+            int result = s.solution(15);
+        System.out.println(result);
+        }
+
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=1; i<=n; i++) {
+            int cnt = 0;
+            for(int j=1; j<=i; j++){
+                if(i%j==0){
+                    cnt++;
+                }
+            }
+            if(cnt >= 3){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
