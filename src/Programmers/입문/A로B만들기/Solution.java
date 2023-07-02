344d73b2d9ed309bbfeb0357a73e80246ef3608c
DOGWANNA <9401ndk@naver.com>
2023-07-02 20:30:33 +0900

    feat: 프로그래머스 A로 B만들기 문제 추가

---
+++ src/Programmers/입문/A로B만들기/Solution.java
@@ -0,0 +1,22 @@
+package Programmers.입문.A로B만들기;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        String result = s.solution("");
+//            int result = s.solution(0);
+        System.out.println(result);
+    }
+
+    public String solution(String letter) {
+        String answer = "";
+        return answer;
+    }
+
+//    public int solution(int letter) {
+//        int answer = 0;
+//
+//        return answer;
+//    }
+}
